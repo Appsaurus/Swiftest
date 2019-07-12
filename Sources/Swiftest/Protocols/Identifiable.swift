8bd5c11bca6bf8d0f5cf312b6188d50d4d64e6ad
Brian Strobach <brian@appsaurus.io>
2019-07-12 09:16:11 -0500

    Default hashable/equatable implementation for Identifiable adopters.

--- Sources/Swiftest/Protocols/Identifiable.swift
+++ Sources/Swiftest/Protocols/Identifiable.swift
@@ -11,3 +11,11 @@ public protocol Identifiable: Hashable {
     associatedtype ID: Hashable
     var id: ID { get }
 }
+public extension Identifiable {
+    var hashValue: Int {
+        return id.hashValue
+    }
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
