bd68270187c7ff096092fc37d3948210eb9cac94
Brian Strobach <brian@appsaurus.io>
2019-10-24 14:44:11 -0500

    Bug fix for specific edge cases when wrapping into range

--- Sources/Swiftest/SwiftStdlib/Extensions/Int/Int+WrappedToRange.swift
+++ Sources/Swiftest/SwiftStdlib/Extensions/Int/Int+WrappedToRange.swift
@@ -9,13 +9,29 @@ import Foundation
 
 public extension Int {
     func wrappedWithin(_ range: ClosedRange<Int>) -> Int {
+        print("Input: \(self) \(range)")
         let min = range.lowerBound
         let max = range.upperBound
         let rangeSize = max - min + 1
-        guard self > 0 else {
-            return max - (min - self).wrappedWithin(range) + 1
+
+        if range.contains(self){
+            return self
         }
-        let value = self % rangeSize
-        return value == 0 ? max : value
+
+        if self < min {
+            print("Distance \(min.distance(to: self))")
+            let remainder = min.distance(to: self).abs % rangeSize
+            if remainder == 0 { return min}
+            let output = (max - remainder) + 1
+            print("Output: \(output)")
+            return output
+        }
+
+         let remainder = max.distance(to: self).abs % rangeSize
+        if remainder == 0 { return max}
+        let output = (min + remainder) - 1
+        print("Output: \(output)")
+        return output
+
     }
 }
--- Tests/SwiftestTests/SwiftStdlibTests/ClosedRangeExtensionTests.swift
+++ Tests/SwiftestTests/SwiftStdlibTests/ClosedRangeExtensionTests.swift
@@ -11,7 +11,13 @@ import XCTest
 final class ClosedRangeExtensionsTests: XCTestCase {
 
     func testWrap() throws {
-        let range = 1...7
+        var range = 1...7
+        XCTAssert(range.wrapToBounds(-15) == 6)
+        XCTAssert(range.wrapToBounds(-14) == 7)
+        XCTAssert(range.wrapToBounds(-13) == 1)
+        XCTAssert(range.wrapToBounds(-12) == 2)
+        XCTAssert(range.wrapToBounds(-11) == 3)
+        XCTAssert(range.wrapToBounds(-10) == 4)
         XCTAssert(range.wrapToBounds(-9) == 5)
         XCTAssert(range.wrapToBounds(-8) == 6)
         XCTAssert(range.wrapToBounds(-7) == 7)
@@ -31,5 +37,34 @@ final class ClosedRangeExtensionsTests: XCTestCase {
         XCTAssert(range.wrapToBounds(7) == 7)
         XCTAssert(range.wrapToBounds(8) == 1)
         XCTAssert(range.wrapToBounds(9) == 2)
+        XCTAssert(range.wrapToBounds(10) == 3)
+        XCTAssert(range.wrapToBounds(11) == 4)
+        XCTAssert(range.wrapToBounds(12) == 5)
+        XCTAssert(range.wrapToBounds(13) == 6)
+        XCTAssert(range.wrapToBounds(14) == 7)
+        XCTAssert(range.wrapToBounds(15) == 1)
+
+        range = 0...7
+
+        XCTAssert(range.wrapToBounds(-10) == 6 )
+        XCTAssert(range.wrapToBounds(-9) == 7)
+        XCTAssert(range.wrapToBounds(-8) == 0)
+        XCTAssert(range.wrapToBounds(-7) == 1)
+        XCTAssert(range.wrapToBounds(-6) == 2)
+        XCTAssert(range.wrapToBounds(-5) == 3)
+        XCTAssert(range.wrapToBounds(-4) == 4)
+        XCTAssert(range.wrapToBounds(-3) == 5)
+        XCTAssert(range.wrapToBounds(-2) == 6)
+        XCTAssert(range.wrapToBounds(-1) == 7)
+        XCTAssert(range.wrapToBounds(0) == 0)
+        XCTAssert(range.wrapToBounds(1) == 1)
+        XCTAssert(range.wrapToBounds(2) == 2)
+        XCTAssert(range.wrapToBounds(3) == 3)
+        XCTAssert(range.wrapToBounds(4) == 4)
+        XCTAssert(range.wrapToBounds(5) == 5)
+        XCTAssert(range.wrapToBounds(6) == 6)
+        XCTAssert(range.wrapToBounds(7) == 7)
+        XCTAssert(range.wrapToBounds(8) == 0)
+        XCTAssert(range.wrapToBounds(9) == 1)
     }
 }
