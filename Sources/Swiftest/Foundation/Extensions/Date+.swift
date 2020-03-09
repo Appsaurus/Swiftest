e40cd1e304ab9349d0cf5a9563acd6b97efa663d
Brian Strobach <brian@appsaurus.io>
2020-03-09 17:33:07 -0500

    Social share refactor

--- Sources/Swiftest/Foundation/Extensions/Date+.swift
+++ Sources/Swiftest/Foundation/Extensions/Date+.swift
@@ -364,7 +364,7 @@ public extension Date {
         // https://github.com/justinmakaila/NSDate-ISO-8601/blob/master/NSDateISO8601.swift
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
 
         return dateFormatter.string(from: self).appending("Z")
